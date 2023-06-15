dbee1008e2e52ee7a503445b848ace8a07ba25f9
marcosmmrk1 <marcospl.2990@gmail.com>
2023-06-15 14:10:55 -0300

    feat: botao

--- src/background/dist/BackGroundAquatico.js
+++ src/background/dist/BackGroundAquatico.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import './BackGroundAquatico.css'
 import { ObjectQuiz } from '../../objectQuiz/ObjectQuiz'
 const Background = () => {
+  const [perguntaQuestNumber,setPerguntaQuestNumber] = useState(0)
+  // const [nextQuestButton,setNextQuestButton] = useState()
+  const nextQuestButtonFunction = () => {
+      setPerguntaQuestNumber(perguntaQuestNumber + 1 )
+      if(perguntaQuestNumber.length< 4 ){
+        alert('tentou')
+      }
+  }
   return (
     <div className="wrapper">
      <h1 className="rectangle">
@@ -12,17 +20,20 @@ const Background = () => {
      src="https://i.pinimg.com/originals/2a/4a/a4/2a4aa468c2b2c418b9c55deb840d6ee9.gif"
      alt="GIF animado"/>
     
-     <h3 className='TituloDaPergunta'>{ObjectQuiz[0].question} </h3>
+     <h3 className='TituloDaPergunta'>{ObjectQuiz[perguntaQuestNumber].question} </h3>
    
     <span className='container'>
  
-     {ObjectQuiz[1].options.map((opcoes,key)=> (
+     {ObjectQuiz[perguntaQuestNumber].options.map((opcoes,key)=> (
       <p key = {key}>
       {console.log(opcoes)}
       <button className='butonOptions'>{opcoes.option}</button>
+      
     </p> 
+    
 
      ))}
+     <button onClick={nextQuestButtonFunction}>disoadioashd</button>
     
     </span>
 
